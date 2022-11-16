a41be708f5b06705bba077d972476fff5c605d6a
Emre-Ozkose <118386840+Emre-Ozkose@users.noreply.github.com>
2022-11-16 21:14:17 +0300

    ilk python denemelerim

---
+++ main.py
@@ -0,0 +1,49 @@
+print("merhaba Etiya")
+# string = metinsel ifade
+text = "10"
+print(text) 
+
+# integer tam sayi
+
+number = 15
+print(number)
+
+# double, flot, decimal = ondalıklı sayı
+
+dnumber = 5.3
+
+#matematiksel operatörler 
+
+print(dnumber)
+print(number + 7)
+print(number - 5)
+print(number / 5)
+
+# mod oparetörü bölümünden kalanı verir
+print(number % 4)
+
+#mantıksal operatörler = her mantıksal operatör boleean değer döner
+#mantıksal karşılaştırma operatörleri
+
+#boleeaon, = true veya folse
+
+print(1 == 1)
+
+
+#string operasyonları
+
+text = "Merhaba Etiya"
+
+print(text.upper())
+print(text.lower())
+
+name = "Mehmet Emre"
+age = "29"
+company = "Etiya"
+
+
+#Emre 29 yaşında Etiya'da çalışıyor
+
+print(name + " " + age +" yaşında "+company+"'da çalışıyor")
+
+#print(name + " " + age + " "+ company "'de calisiyor")
